640343538975a252eec17d059e00effc2bdb2d89
Artur Filatov <serawwerastat@gmail.com>
2019-07-24 15:28:47 +0300

    Back from user guide

--- app/src/main/java/com/example/blind_map_v3/UserGuide.java
+++ app/src/main/java/com/example/blind_map_v3/UserGuide.java
@@ -2,7 +2,10 @@ package com.example.blind_map_v3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.github.barteksc.pdfviewer.PDFView;
 
@@ -19,5 +22,24 @@ public class UserGuide extends AppCompatActivity {
         userguide = (PDFView) findViewById(R.id.pdfuserguide);
         userguide.fromAsset("userguide.pdf").load();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item2, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.back:
+                Intent i = new Intent(UserGuide.this, MainActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
